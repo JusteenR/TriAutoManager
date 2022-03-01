de638da8d11a88fb642aaed52d3f96985dc98187
JusteenR <justeen.randev@gmail.com>
2022-03-01 18:26:57 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,17 +1,54 @@
+
 // Importing express module
 const express = require('express');
 const app = express();
- 
-// Getting Request
-app.get('/', (req, res) => {
- 
-    // Sending the response
-    res.send('Hello World!!!')
+const path = require("path");
+const fs = require("fs");
+
+// constants
+const DB_PATH = path.resolve("db.json");
+
+app.use(express.json());
+
+// routes
+app.get("/", async (req, res) => {
+  fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
+    if (err) return console.log("Error in reading from db");
+    let values = JSON.parse(jsonString);
+    res.status(200).json({
+      totalValues: values.length,
+      values,
+    });
+  });
+
+  // Sending the response
+  res.send('Hello World!!!')
     
-    // Ending the response
-    res.end()
-})
- 
+  // Ending the response
+  res.end()
+
+});
+app.post("/", async (req, res) => {
+  fs.readFile(DB_PATH, "utf-8", (err, jsonString) => {
+    if (err) return console.log("Error in reading from db");
+    let body = req.body;
+    let valuesArr = JSON.parse(jsonString);
+    let obj = {
+      temperature: body.temperature,
+      humidity: body.humidity,
+      timestamp: new Date(),
+    };
+    valuesArr.push(obj);
+    fs.writeFile(DB_PATH, JSON.stringify(valuesArr), (err) => {
+      if (err) return console.log("Error in updating db");
+      res.status(200).json({
+        message: "Values saved",
+        value: valuesArr[valuesArr.length - 1],
+      });
+    });
+  });
+});
+
 // Establishing the port
 const PORT = process.env.PORT ||5000;
  
