d4517e6b573a20b5990621521823541aa4df0f75
JusteenR <justeen.randev@gmail.com>
2022-04-05 17:06:15 -0400

    Trying to fix connect lost

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post('/vitals', (req, res) => {
 });
 
 // Establishing the port
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 8080;
  
 // Executing the server on given port number
 app.listen(PORT, console.log(
